9a83153a514e86b2185f7363aedae797cbcda085
Mehmet Eroglu <mehmet---eroglu@hotmail.com>
2017-05-25 13:29:36 +0300

    Initial Commit

---
+++ Pokedex-by-MehmetEroglu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-by-MehmetEroglu
+//
+//  Created by Mehmet Eroğlu on 25.05.2017.
+//  Copyright © 2017 Mehmet Eroğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
